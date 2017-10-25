205131bac60d24181e0b4c603a1e07ebd9d1eb7d
priyanka5693 <32127443+priyanka5693@users.noreply.github.com>
2017-10-25 12:24:27 +0530

    Update Atos_Innovatos2017.R

--- Atos_Innovatos/Atos_Innovatos2017.R
+++ Atos_Innovatos/Atos_Innovatos2017.R
@@ -2,8 +2,7 @@
 ############################################################
 # Title: Data Analytics App Demo for the Atos Innovatos 2017 event
 # 
-# Author: Marcel van den Bosch <marcel.vandenbosch@atos.net>
-# Date: 07-Mar-2017
+
 #
 # Copyright 2017: Atos Consulting & Atos Codex
 # 
